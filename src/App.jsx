8cfaadf9a6a872eee73a28a2dfc8b715f532d366
Santiago Gaviria <gaviriasantiago1997@gmail.com>
2025-05-15 07:31:48 -0500

    desarrollo finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,103 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
+import { useState, useEffect } from "react";
+import { db } from "./data/db";
+import Guitar from "./components/Guitar";
+import Footer from "./shared/Footer";
+import Header from "./shared/Header";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem("cart");
+    return localStorageCart ? JSON.parse(localStorageCart) : [];
+  };
+
+  const [data, setdata] = useState([]);
+  const [cart, setCart] = useState(initialCart);
+
+  useEffect(() => {
+    setdata(db);
+  }, []);
+
+  useEffect(() => {
+    if (cart.length > 0) {
+      saveLocalStorage();
+      return;
+    }
+    saveLocalStorage();
+  }, [cart]);
+
+  const addToCart = (guitar) => {
+    const itemExists = cart.findIndex((item) => item.id === guitar.id);
+
+    if (itemExists < 0) {
+      guitar.quantity = 1;
+      setCart([...cart, guitar]);
+      return;
+    }
+
+    if (cart[itemExists].quantity >= 5) {
+      return;
+    }
+    const updateCart = [...cart];
+    updateCart[itemExists].quantity++;
+    setCart(updateCart);
+  };
+
+  const removeFromCart = (id) => {
+    setCart((prevCart) => prevCart.filter((item) => item.id != id));
+  };
+
+  const clearCart = () => {
+    setCart([]);
+  };
+
+  const saveLocalStorage = () => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  };
+
+  const increaseAndDecreaseQuantity = (id, type) => {
+    const updateCart = cart.map((item) => {
+      if (item.id == id) {
+        if (!type && item.quantity > 1) {
+          item.quantity--;
+        }
+
+        if (type && item.quantity < 5) {
+          item.quantity++;
+        }
+      }
+      return item;
+    });
+    setCart(updateCart);
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>hola mundo en react</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseAndDecreaseQuantity={increaseAndDecreaseQuantity}
+        clearCart={clearCart}
+      ></Header>
+
+      <main className="container-xl mt-5">
+        <h2 className="text-center">Nuestra Colección</h2>
+
+        <div className="row mt-5">
+          {data.length != 0 ? (
+            data.map((item) => (
+              <Guitar
+                key={item.id}
+                guitar={item}
+                addToCart={addToCart}
+              ></Guitar>
+            ))
+          ) : (
+            <></>
+          )}
+        </div>
+      </main>
+
+      <Footer></Footer>
     </>
   );
 }
